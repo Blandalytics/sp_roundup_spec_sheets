93d83fae8217c1a5def7bc4d6bf5ec74469d52b7
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-07-02 13:41:02 -0700

    Update test_sheet_viewer.py

--- test_sheet_viewer.py
+++ test_sheet_viewer.py
@@ -6,7 +6,7 @@ import requests
 
 html_path = 'https://html-preview.github.io/?url=https://github.com/Blandalytics/sp_roundup_spec_sheets/blob/main/7_1_25_spec_sheet.html'
 
-with open(html_path,'r') as f: 
-    html_data = f.read()
+# with open(html_path,'r') as f: 
+#     html_data = f.read()
 
-st.html(html_data)
+st.html(html_path)
