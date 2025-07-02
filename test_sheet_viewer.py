4567f4fa93b3f5fea4c3587bce051a3502cf8683
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-07-02 13:31:39 -0700

    Update test_sheet_viewer.py

--- test_sheet_viewer.py
+++ test_sheet_viewer.py
@@ -4,4 +4,9 @@ import numpy as np
 import pandas as pd
 import requests
 
-st.html('https://github.com/Blandalytics/sp_roundup_spec_sheets/blob/main/7_1_25_spec_sheet.html?raw=true')
+html_path = 'https://github.com/Blandalytics/sp_roundup_spec_sheets/blob/main/7_1_25_spec_sheet.html?raw=true'
+
+with open(html_path,'r') as f: 
+    html_data = f.read()
+
+st.html(html_data)
