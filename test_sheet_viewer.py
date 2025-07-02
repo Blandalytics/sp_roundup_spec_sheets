b9b61641d4c3640d26e78f752bb8ecd993e1c370
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-07-02 08:33:12 -0700

    Update test_sheet_viewer.py

--- test_sheet_viewer.py
+++ test_sheet_viewer.py
@@ -3,6 +3,5 @@ import datetime
 import numpy as np
 import pandas as pd
 import requests
-import seaborn as sns
 
 st.html('https://github.com/Blandalytics/sp_roundup_spec_sheets/blob/main/7_1_25_spec_sheet.html?raw=true')
