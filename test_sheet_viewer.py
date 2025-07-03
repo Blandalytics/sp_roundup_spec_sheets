828163a0bf3e3146a5c50ae570c0d36734e99ea6
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-07-02 23:07:21 -0700

    Update test_sheet_viewer.py

--- test_sheet_viewer.py
+++ test_sheet_viewer.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import datetime
-import numpy as np
-import pandas as pd
-import requests
 
 html_path = 'https://html-preview.github.io/?url=https://github.com/Blandalytics/sp_roundup_spec_sheets/blob/main/7_1_25_spec_sheet.html'
 
